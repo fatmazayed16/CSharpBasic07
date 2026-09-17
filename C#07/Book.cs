6b8cd0298aca550e8c78b6cead422da6a2f01c84
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-17 21:24:11 +0300

    Q2

--- C#07/Book.cs
+++ C#07/Book.cs
@@ -4,6 +4,10 @@ internal class Book
 {
 	#region Q1 field
 	private string Password = "secret";
+	#endregion
+
+	#region Q2 Field
+	internal int copiesInStock = 5;
     #endregion
 
 }
--- C#07/Program.cs
+++ C#07/Program.cs
@@ -7,9 +7,13 @@
             #region Q1
             Book book = new Book();
             //Console.WriteLine(book.Password);
-            //// Password is private, so it cannot be accessed from outside the Book class.
+            ////It won't compile cuz Password is private, so it cannot be accessed from outside the Book class.
             #endregion
 
+            #region Q2
+            Console.WriteLine(book.copiesInStock);
+            //It will combile cuz copiesInStock is internal, so so it can be accessed from outside the Book class(but in the same project).
+            #endregion
 
         }
     }
