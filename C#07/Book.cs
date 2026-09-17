7f6a1dc1aa6e07b738f7cb95cffcd6512a6ddf77
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-17 21:15:51 +0300

    Add project files.

---
+++ C#07/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace C_07;
+
+internal class Book
+{
+    private string Password = "secret";
+
+}
---
+++ C#07/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            //Console.WriteLine(book.Password);
+            //// Password is private, so it cannot be accessed from outside the Book class.
+            
+
+        }
+    }
+}
